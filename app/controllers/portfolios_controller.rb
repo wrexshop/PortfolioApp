30adac1003656136db216af4c3d502dfb09118b3
justin <ramos.justin21@gmail.com>
2018-05-29 14:48:11 -0700

    Displayed portfolio data in portfolios index page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,8 @@
 class PortfoliosController < ApplicationController
+  # Remember: before_actoin is only for update show delete edit
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+  
 end
