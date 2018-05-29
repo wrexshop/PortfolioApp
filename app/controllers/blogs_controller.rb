ba59196e9fbb2ff252cf084d1d24f0f7e7666cc3
justin <ramos.justin21@gmail.com>
2018-05-29 15:35:50 -0700

    Integrated edit/udpate for portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog_id, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
   def index
@@ -53,7 +53,7 @@ class BlogsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_blog
+    def set_blog_id
       @blog = Blog.find(params[:id])
     end
 
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
-  # Remember: before_actoin is only for update show delete edit
+  # Remember: before_action is only for update show delete edit
+  before_action :set_portfolio_id, only: [:edit, :update]
 
   def index
     @portfolio_items = Portfolio.all
@@ -22,12 +23,30 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(allowed_portfolio_params)
+        # Redirect to index page
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
 
   def allowed_portfolio_params
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
                                       :body)
+  end
+
+  def set_portfolio_id
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
