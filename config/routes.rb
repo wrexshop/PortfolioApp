317a7527bf501bc79a142eaa08fe762dadc8e72f
justin <ramos.justin21@gmail.com>
2018-05-29 11:51:16 -0700

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # Pages routes
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
+  # Blogs route
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
