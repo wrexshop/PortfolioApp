48a5ea5051935a35f281f7d353d5df52519d3193
justin <ramos.justin21@gmail.com>
2018-06-11 13:53:31 -0700

    Moved devise whitelist to a concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-
-    # This will only run if it interacting with the devise controller
-    before_action :configure_permitted_parameter, if: :devise_controller?
-
-    # This is specific to rails 5
-    def configure_permitted_parameter
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  include ConcernDeviseWhitelist
 end
