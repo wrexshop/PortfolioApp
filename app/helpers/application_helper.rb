a99c8e1168974f2604b67974e662a4161efb621c
justin <ramos.justin21@gmail.com>
2018-06-13 15:24:25 -0700

    Implemented source helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   
+  def sample_helper
+    content_tag(:div, "Sample Helper!")
+  end
+
   def login_helper
     if current_user.is_a?(OpenStruct)
       (login = link_to "Login", new_user_session_path) +
@@ -7,6 +11,14 @@ module ApplicationHelper
       (register = link_to "Register", new_user_registration_path)
     else
       link_to "Log out", destroy_user_session_path, method: :delete
+    end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}" +
+                  " and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
 
