b14932baaea1e536787efdd3e909add9534d34e6
justin <ramos.justin21@gmail.com>
2018-06-15 15:07:56 -0700

    updated user edit form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,13 @@ module ApplicationHelper
     content_tag(:div, "Sample Helper!")
   end
 
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (login = link_to "Login", new_user_session_path) +
-      "<br>".html_safe + 
-      (register = link_to "Register", new_user_registration_path)
+      (login = link_to "Login", new_user_session_path, class: style) +
+      " ".html_safe + 
+      (register = link_to "Register", new_user_registration_path, class: style)
     else
-      link_to "Log out", destroy_user_session_path, method: :delete
+      link_to "Log out", destroy_user_session_path, method: :delete, class: style
     end
   end
 
